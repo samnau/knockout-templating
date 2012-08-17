a7d67f8e8acb293b3aa67e6d609e4f7d7c67368b
Sam Nau <snau@vml.com>
2012-08-17 14:06:55 -0500

    Initial upload of project files

---
+++ HTML/js/example1.js
@@ -0,0 +1,34 @@
+myViewModel = {
+		type : ko.observable('man'),
+		mood : ko.observable('happy'),
+		getMoody: ko.observable(false),
+		multiType : ko.observable('0'),
+		templateTypes: ['man','monkey','monster'],
+		moodTypes : ['happy','sad','angry','jealous','sleepy','hungry', 'eeeeeevil!!'],
+		members : [
+			{
+				name: 'Harvey',
+				type: ko.observable('man'),
+				mood : ko.observable('happy')
+			},
+			{
+				name: 'Simon',
+				type: ko.observable('monkey'),
+				mood : ko.observable('eeeeeevil!!')
+			},
+			{
+				name: 'Jarvis',
+				type: ko.observable('monster'),
+				mood : ko.observable('hungry')
+			},
+			{
+				name: 'Alvin',
+				type: ko.observable('man'),
+				mood : ko.observable('jealous')
+			}
+		]
+	}
+	
+	jQuery(function($) {
+    ko.applyBindings(myViewModel);
+  });
